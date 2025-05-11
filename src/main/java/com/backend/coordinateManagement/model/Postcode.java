d2e97ce1fb6fcf14342c5371106afa85ff8f3fcd
sebastianlee90 <sebastianlkc90@gmail.com>
2025-05-11 20:12:48 +0800

    Update Model for Postcode

---
+++ src/main/java/com/backend/coordinateManagement/model/Postcode.java
@@ -0,0 +1,27 @@
+package com.backend.coordinateManagement.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity(name = "rm_category")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Postcode {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "postcode")
+    private String postcode;
+
+    @Column(name = "latitude")
+    private String latitude;
+
+    @Column(name = "longtitude")
+    private String longitude;
+}
