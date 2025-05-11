18995bab304c184c59c2a166d2d5056a3c5040bb
sebastianlee90 <sebastianlkc90@gmail.com>
2025-05-11 20:07:15 +0800

    Remove unncessary comments

--- src/main/java/com/backend/coordinateManagement/services/impl/PostcodeService.java
+++ src/main/java/com/backend/coordinateManagement/services/impl/PostcodeService.java
@@ -16,27 +16,8 @@ import java.util.List;
 public class PostcodeService implements IPostcodeService {
     private final PostcodeRepositoryJooq postcodeRepositoryJooq;
 
-    // Read data from the CSV file
     public List<PostcodeDTO> getPostcodeList() {
         log.info(LogUtil.ENTRY_SERVICES, "getCategoryList");
         return postcodeRepositoryJooq.getPostcodeList();
     }
-
-    // Write data to the CSV file
-    // public void writeCsv(List<PostcodeOutcode> postcodeOutcodes) throws IOException {
-    //     try (CSVWriter writer = new CSVWriter(new FileWriter(CSV_FILE_PATH))) {
-    //         // Write the header row
-    //         writer.writeNext(new String[]{"id", "postcode", "latitude", "longitude"});
-
-    //         // Write each record
-    //         for (PostcodeOutcode postcodeOutcode : postcodeOutcodes) {
-    //             writer.writeNext(new String[]{
-    //                     String.valueOf(postcodeOutcode.getId()),
-    //                     postcodeOutcode.getPostcode(),
-    //                     String.valueOf(postcodeOutcode.getLatitude()),
-    //                     String.valueOf(postcodeOutcode.getLongitude())
-    //             });
-    //         }
-    //     }
-    // }
 }
